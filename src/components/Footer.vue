6a7192a39333207f6a2da547b67166f1b796f234
Davs06 <santosdv40@gmail.com>
2023-04-14 15:02:30 -0300

    Criando scopo do projt e config rotas home/pedid

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <footer>
+            footer
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Footer'
+}
+</script>
+
+<style scoped>
+
+</style>
+
